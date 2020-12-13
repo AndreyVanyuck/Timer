b6df7db4708ecb5dc3777a88c9a08a7812404943
AndreyVanyuck <avanyuck@gmail.com>
2020-12-13 23:16:04 +0300

    added db instance

--- app/src/main/java/com/example/timerppo/MainActivity.java
+++ app/src/main/java/com/example/timerppo/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.timerppo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.timerppo.DB.DatabaseHandler;
+import com.example.timerppo.DB.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    DatabaseHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = DatabaseHandler.getInstance().getDatabase();
+
+        listView = findViewById(R.id.listTimer);
+        //TODO
+        //Set adapter
+
     }
 }
