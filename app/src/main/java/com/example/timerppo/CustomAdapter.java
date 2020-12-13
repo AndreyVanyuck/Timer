781ce1961c400f104c7216c788b8217dcbcdd85c
AndreyVanyuck <avanyuck@gmail.com>
2020-12-14 00:31:22 +0300

    added ViewHolder

--- app/src/main/java/com/example/timerppo/CustomAdapter.java
+++ app/src/main/java/com/example/timerppo/CustomAdapter.java
@@ -1,8 +1,14 @@
 package com.example.timerppo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.timerppo.DB.DatabaseHelper;
 import com.example.timerppo.Models.TimerModel;
@@ -22,5 +28,18 @@ public class CustomAdapter extends ArrayAdapter<TimerModel> {
         this.layout = resource;
         this.db = db;
         this.inflater = LayoutInflater.from(context);
+    }
+
+    private class ViewHolder {
+        Button removeButton, editButton, startButton;
+        TextView nameView;
+        LinearLayout layout;
+        ViewHolder(View view){
+            removeButton = view.findViewById(R.id.removeButton);
+            editButton = view.findViewById(R.id.editButton);
+            startButton = view.findViewById(R.id.startButton);
+            nameView = view.findViewById(R.id.nameView);
+            layout = view.findViewById(R.id.layout);
+        }
     }
 }
