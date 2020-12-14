2da605e1dc6b5f9b46ec1715c6877c70cf8a2015
AndreyVanyuck <avanyuck@gmail.com>
2020-12-14 13:33:22 +0300

    changed TimerDao class to interface, add getAll query

--- app/src/main/java/com/example/timerppo/DB/TimerDao.java
+++ app/src/main/java/com/example/timerppo/DB/TimerDao.java
@@ -1,4 +1,14 @@
 package com.example.timerppo.DB;
 
-public class TimerDao {
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.example.timerppo.Models.TimerModel;
+
+import java.util.List;
+
+@Dao
+public interface TimerDao {
+    @Query("SELECT * FROM timerModel")
+    List<TimerModel> getAll();
 }
