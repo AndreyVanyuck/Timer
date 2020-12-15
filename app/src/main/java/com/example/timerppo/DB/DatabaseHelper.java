9bdb454f8b2d6397f6fc970288c8027d21b98576
AndreyVanyuck <avanyuck@gmail.com>
2020-12-15 18:58:58 +0300

    added Dao interface

--- app/src/main/java/com/example/timerppo/DB/DatabaseHelper.java
+++ app/src/main/java/com/example/timerppo/DB/DatabaseHelper.java
@@ -2,11 +2,10 @@ package com.example.timerppo.DB;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 
-import com.example.timerppo.Models.TimerModel;
+import com.example.timerppo.Models.WorkoutModel;
 
-@Database(entities = {TimerModel.class}, version = 1)
+@Database(entities = {WorkoutModel.class}, version = 1)
 public abstract class DatabaseHelper extends RoomDatabase {
-    public abstract TimerDao timerDao();
+    public abstract WorkoutDao timerDao();
 }
---
+++ app/src/main/java/com/example/timerppo/DB/WorkoutDao.java
@@ -0,0 +1,18 @@
+package com.example.timerppo.DB;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.timerppo.Models.WorkoutModel;
+
+import java.util.List;
+
+@Dao
+public interface WorkoutDao {
+    @Query("SELECT * FROM workoutmodel")
+    List<WorkoutModel> getAll();
+
+    @Insert
+    void insert(WorkoutModel workoutModel);
+}
