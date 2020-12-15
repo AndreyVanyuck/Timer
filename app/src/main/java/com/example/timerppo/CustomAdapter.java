e15512a1c353e50110aebfd6796012e3dcf7a815
AndreyVanyuck <avanyuck@gmail.com>
2020-12-15 19:09:04 +0300

    changed extends class from ArrayAdapter<> to BaseAdapter

--- app/src/main/java/com/example/timerppo/CustomAdapter.java
+++ app/src/main/java/com/example/timerppo/CustomAdapter.java
@@ -4,32 +4,48 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 
 
 import com.example.timerppo.DB.DatabaseHelper;
-import com.example.timerppo.Models.TimerModel;
+import com.example.timerppo.Models.WorkoutModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CustomAdapter extends ArrayAdapter<TimerModel> {
-    private LayoutInflater inflater;
+public class CustomAdapter extends BaseAdapter {
     private int layout;
-    private List<TimerModel> timerModelList;
+    private List<WorkoutModel> workoutModelList = new ArrayList<WorkoutModel>();
     private DatabaseHelper db;
+    private Context context;
 
-    CustomAdapter(Context context, int resource, List<TimerModel> timerModels,
-                 DatabaseHelper db) {
-        super(context, resource, timerModels);
-        this.timerModelList = timerModels;
+    CustomAdapter(Context context, int resource, List<WorkoutModel> workoutModels,
+                  DatabaseHelper db) {
+        this.context = context;
+        this.workoutModelList = workoutModels;
         this.layout = resource;
         this.db = db;
-        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return workoutModelList.size();
+    }
+
+    @Override
+    public WorkoutModel getItem(int position) {
+        return workoutModelList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return workoutModelList.get(position).getId();
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder;
         if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(this.layout, parent, false);
             viewHolder = new ViewHolder(convertView);
             convertView.setTag(viewHolder);
@@ -37,22 +53,21 @@ public class CustomAdapter extends ArrayAdapter<TimerModel> {
         else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        TimerModel timerModel = timerModelList.get(position);
-        viewHolder.nameView.setText(timerModel.Name);
-        viewHolder.idView.setText((Integer.toString(timerModel.Id)));
-        viewHolder.layout.setBackgroundColor(timerModel.Color);
-
+        WorkoutModel workoutModel = workoutModelList.get(position);
+        viewHolder.nameView.setText(workoutModel.getName());
+        viewHolder.idView.setText((Integer.toString(workoutModel.getId())));
+        viewHolder.layout.setBackgroundColor(workoutModel.getColor());
+/*
         //TODO
         viewHolder.startButton.setOnClickListener(i -> {
         });
-
         //TODO
         viewHolder.removeButton.setOnClickListener(i -> {
         });
-
         //TODO
         viewHolder.editButton.setOnClickListener(i -> {
         });
+*/
 
         return convertView;
     }
