5cfb340745f92d7eddeec5bedc92c1d6d4451a47
AndreyVanyuck <avanyuck@gmail.com>
2020-12-15 19:41:21 +0300

    deleted add_button listener

--- app/src/main/java/com/example/timerppo/MainActivity.java
+++ app/src/main/java/com/example/timerppo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import com.example.timerppo.DB.DatabaseHandler;
@@ -25,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
         CustomAdapter adapter = new CustomAdapter(this, R.layout.list_item, db.timerDao().getAll(), db);
         listView.setAdapter(adapter);
 
-        findViewById(R.id.buttonAddTimer).setOnClickListener(i -> {
-            //Intent intent = new Intent(getApplicationContext(), CreateTimer.class);
-           // startActivity(intent);
+        findViewById(R.id.buttonAddTimer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateWorkoutActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
